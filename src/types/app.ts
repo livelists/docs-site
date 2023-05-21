a87be63802189b43cd449b1781cc694cc9ff0456
Никита Крайнев <37338741+WebDevRu@users.noreply.github.com>
2023-05-21 13:01:34 +0300

    remove less

--- src/types/app.ts
+++ src/types/app.ts
@@ -1,15 +1,6 @@
 import { AppLanguages } from 'const/app/AppLanguages';
-import { BaseCurrencies } from 'const/converter/BaseCurrencies';
 
 export interface IAppState {
     language: AppLanguages,
     isInit: boolean,
-}
-
-export interface ILatestRatesResponse {
-    disclaimer: string,
-    license?: string,
-    timestamp: number,
-    base?: BaseCurrencies,
-    rates: Record<string, number>
 }

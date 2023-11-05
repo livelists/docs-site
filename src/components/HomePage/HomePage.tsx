18973ee259eac3b5234835e88b5bd5198342e9e9
nikitakrainev <nikrainev@gmail.com>
2023-11-06 00:49:15 +0300

    fix: chat container layout

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -3,11 +3,14 @@ import React from 'react';
 import { Card, CardBody } from '@chakra-ui/react';
 
 import { Header } from './Header';
+import styles from './HomePage.module.scss';
 
 const HomePage:React.FC = () => {
     return (
         <Card>
-            <CardBody>
+            <CardBody
+                className={styles.card}
+            >
                 <Header />
             </CardBody>
         </Card>
